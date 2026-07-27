f2905afc236feac2a752713927c7c4e4df3e7f87
Yatharth Singhai <yatharthsinghai7@gmail.com>
2026-07-27 21:46:41 +0530

    fix: load environment variables in api/main.py before startup validations

--- api/main.py
+++ api/main.py
@@ -7,6 +7,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
+from dotenv import load_dotenv
+
+# Load env variables
+load_dotenv()
 
 # Import local packages
 from api.middleware import limiter, request_logger_middleware, logger
